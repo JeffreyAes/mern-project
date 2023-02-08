4c5ddd76bba25f096c4f3d51a8a51811a2c89029
Jeffrey <aeschlimanjeffrey@gmail.com>
2023-02-07 21:19:26 -0800

    randomply pick an image from gallery to preview

--- scribble/client/src/views/Profile.jsx
+++ scribble/client/src/views/Profile.jsx
@@ -6,6 +6,7 @@ const Profile = (props) => {
     const { user_id } = useParams();
     const [gallerys, setGallerys] = useState();
     const [loaded, setLoaded] = useState(false);
+    const [artist, setArtist] = useState("")
     useEffect(() => {
         axios.get('http://localhost:8000/api/gallery/user/' + user_id)
             .then(res => {
@@ -14,43 +15,45 @@ const Profile = (props) => {
             })
             .catch(err => console.error(err));
     }, []);
+    useEffect(() => {
+        axios.get('http://localhost:8000/api/users/' + user_id)
+            .then(res => {
+                setArtist(res.data);
+                setLoaded(true);
+            })
+            .catch(err => console.error(err));
+    }, []);
 
 
     return (
         <div className='container mt-3'>
+            <h1>{artist.username}</h1>
             <div className='d-flex gap-3'>
-            <Link to={`/dashboard/${user_id}`} >dashboard</Link>
+                <Link to={`/dashboard/${user_id}`} >dashboard</Link>
                 <Link to={`/gallery/create/${user_id}`}>Add a Gallery</Link>
-            <Logout />
+                <Logout />
             </div>
             <div className="text-center">
-
-                
                 <div>
                     <div className="col-sm-12">
-
-
-
                         <div className="d-flex flex-wrap " >
-                        {loaded && gallerys.map((gallery, i) =>
-
+                            {loaded && gallerys.map((gallery, i) =>
                                 <div className="border p-2 m-3 shadow rounded " key={i}>
-                                    <h1>blank for now</h1>
+                                        <img src={gallery.collectionList[Math.floor(Math.random() * 
+                                            (gallery.collectionList.length - 0))]?.image}
+                                            alt="Empty gallery" width={'150px'} height={'150px'} />
                                     <div>
-
                                         <h5> <Link to={`/gallery/${gallery._id}`}>{gallery.collectionTitle}</Link> </h5>
                                         <h5 >{gallery.about}</h5>
                                         <h5> tbd </h5>
                                     </div>
                                 </div>
-                        )}
+                            )}
                         </div>
-                        </div>
+                    </div>
                 </div>
-                </div>
-
             </div>
-            )
+        </div>
+    )
 }
-
-            export default Profile
+export default Profile
